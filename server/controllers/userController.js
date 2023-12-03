a9bffd8d13b71c60fc3e4c187d10f79e1e60b860
Oleg <olegatortor111@gmail.com>
2023-12-03 16:31:41 +0200

    add user logic BE

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,22 +1,57 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некоректний пароль чи пошта'))
+        }
+        const candidate = await User.findOne({where: {email}})
 
+        if(candidate){
+            return next(ApiError.badRequest('Користувач вже зареєстрований'))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+
+        return res.json({token})
     }
 
     
-    async login(req, res) {
-        
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if(!user) {
+            return next(ApiError.internal('Користувач не знайдений'))
+        }
+        let comparePassword= bcrypt.compareSync(password, user.password)
+
+        if(!comparePassword) {
+            return next(ApiError.internal('Неправильний пароль, спробуйте ще раз'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+
+        return res.json({token})
     }
 
     
     async chek(req, res, next) {
-        const {id} = req.query
-        if(!id) {
-            return next(ApiError.badRequest('Не введений ID'))
-        }
-        res.json(id)
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 
--- server/routes/productsRouter.js
+++ server/routes/productsRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router;
 const productsController = require('../controllers/productsController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', productsController.create)
+router.post('/', checkRole('ADMIN'), productsController.create)
 router.get('/', productsController.getAll)
 // router.get('/:id', productsController.getOne)
 
